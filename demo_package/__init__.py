2b69570e9929492e5f0578dcca1f911b2786d90c
xiaocai <1170613300@qq.com>
2020-01-17 17:16:29 +0800

    Success

---
+++ demo_package/__init__.py
@@ -0,0 +1,6 @@
+
+def demo():
+    print("This is a test package demo!")
+
+if __name__=='__main__':
+    demo()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(
+    name="demo_package",
+    version="0.1",
+    py_modules=['demo_package'],
+)
